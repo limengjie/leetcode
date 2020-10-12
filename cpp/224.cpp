3fbf9647fcbd190a8e20d0166f9295b7a314ef22
Mengjie Li <mli55@unix2.lt.ucsc.edu>
2020-10-11 19:22:26 -0700

    basic calculator

--- cpp/224.cpp
+++ cpp/224.cpp
@@ -1,43 +1,68 @@
 #include <iostream>
-#include "linkedList.h"
+#include <string>
+#include <stack>
 
 using namespace std;
 
-List rmDupList(List list) {
-	List res;
-	if (list.getHead() == nullptr) 
-		return res;
+//basic calculator
+int calc(stack<int>& stk)
+{
+    int sum = 0;
+    while (!stk.empty()) {
+        sum += stk.top();
+        stk.pop();
+    }
+            
+    return sum;
+}
 
-	Node * prevNode = list.getHead();
-	Node * pNode = prevNode->getNext();
-	res.append(prevNode->getVal());
-	while (pNode != nullptr) {
-		cout << "prev = " << prevNode->getVal();
-		cout << ", cur = " << pNode->getVal() << endl;
-		if (pNode->getVal() != prevNode->getVal()) {
-				//link two nodes
-				prevNode->setNext(pNode);
-				//save the result
-				res.append(pNode->getVal());
-				//update prev node
-				prevNode = pNode;
-		}
-		//update the current node
-		pNode = pNode->getNext();
-	}
-	//update the tail
-	res.setTail(prevNode);
+int helper(string s, size_t& i)
+{
+    stack<int> stk;
+    int sign = 1;
+    int num = 0;
+    for (; i < s.size(); ++i)
+    {
+        if (isdigit(s[i]))
+            num = num * 10 + (s[i] - '0');
+        else if (s[i] != ' ') // '(', ')', '+' ,'-'
+        {
+            switch(s[i])
+            {
+                case '(':
+                    num = helper(s, ++i);
+                    break;
+                case '+':
+                    stk.push(num * sign);
+                    num = 0;
+                    sign = 1;
+                    break;
+                case '-':
+                    stk.push(num * sign);
+                    num = 0;
+                    sign = -1;
+                    break;
+                case ')':
+                    stk.push(num * sign);
+                    return calc(stk);
+            }
+        }
+        if (i == s.size() - 1)
+	    stk.push(num * sign);
+    }
+    return calc(stk);
+}
 
-	return res;
+int calculate(string s) {
+   if (s.empty()) return 0;
+   size_t i = 0;
+   return helper(s, i);
 }
 
 int main() {
-	vector<int> vec = {1, 2, 2,  3, 3, 3, 3};
-	List l = buildList(vec);
+   string s {"(1+(5+4+2)-3)+(6+8)"};
+   //string s {"(2+3+4)+3"};
+   cout << calculate(s) << endl;
 
-	List res = rmDupList(l);
-
-	res.show();
-
-	return 0;
+   return 0;
 }
